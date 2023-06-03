b0febfcd5879fbfa18d91d2dd37bc80f9d096c4f
lordsynix <leSynix22@gmail.com>
2023-06-03 12:24:44 +0200

    backend

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const keys = require('./config/keys.js');
+const app = express();
+
+// Setting up DB
+const mongoose = require('mongoose');
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Setup database models
+require('./model/Account');
+
+// Setup the routes
+require('./routes/authenticationRoutes')(app);
+
+app.listen(keys.port, () => {
+    console.log("Listening on " + keys.port);
+});
